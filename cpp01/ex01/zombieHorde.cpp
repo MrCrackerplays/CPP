6581f87421871af3f042ef5f48e02ac2745c5e9c
pdruart <pdruart@student.codam.nl>
2022-08-17 13:40:38 +0200

    updated capitalization of zombieHorde file

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -1,6 +1,6 @@
 #include "Zombie.hpp"
 #include <sstream>
-
+ 
 Zombie	*zombieHorde(int N, std::string name) {
 	Zombie				*horde;
 	std::ostringstream	oss;//streams are freaky ngl, but are allowed as they're not part of STL
