7fc1e1d5f9bf853fc935fd1c0e6c9cbc1ae8fdd4
pdruart <pdruart@student.codam.nl>
2022-08-18 18:05:00 +0200

    retrofitted code to no longer be inclusive on triangle edges (thanks updated subject, I hated doing that)

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -7,39 +7,61 @@ Fixed get_y_for_x(Point const p1, Point const p2, Fixed const x) {
 	return (expectedY);
 }
 
-bool	is_on_under(Point const p1, Point const p2, Point const p3, Point const p) {
+bool	is_under(Point const p1, Point const p2, Point const p3, Point const p) {
 	//handling straight line where p1 and p2 are above one another because then you'd get division by 0 problems
 	if (p1.getX() == p2.getX()) {
+		if (p1.getX() == p.getX())
+			return (false);
 		if (p1.getX() == p3.getX()) {
-			if (p1.getX() != p.getX())
-				return (false);
-			if (p.getY() > Fixed::max(Fixed::max(p1.getY(), p2.getY()), p3.getY()))
-				return (false);
-			if (p.getY() < Fixed::min(Fixed::min(p1.getY(), p2.getY()), p3.getY()))
-				return (false);
-			return (true);
+			return (false);
+			//the subject *used* to accept point on the edge of the triangle too but no longer, thus it'll always be false if the triangle is a line (since a point will always be on the edge of a line)
+			// if (p1.getX() != p.getX())
+			// 	return (false);
+			// if (p.getY() > Fixed::max(Fixed::max(p1.getY(), p2.getY()), p3.getY()))
+			// 	return (false);
+			// if (p.getY() < Fixed::min(Fixed::min(p1.getY(), p2.getY()), p3.getY()))
+			// 	return (false);
+			// return (true);
 		}
 		else {
-			bool right_p3 = (p1.getX() - p3.getX()) < Fixed(0);
-			bool right_p = (p1.getX() - p.getX()) < Fixed(0);
-			return (right_p3 == right_p);
+			bool p3_same_side_p = ((p3.getX() < 0) == (p.getX() < 0));
+			Fixed distance_from_p_to_p1 = p1.getX() > p.getX() ? ((Fixed)p1.getX() - (Fixed)p.getX()) : ((Fixed)p.getX() - (Fixed)p1.getX());
+			Fixed distance_from_p3_to_p1 = p1.getX() > p3.getX() ? ((Fixed)p1.getX() - (Fixed)p3.getX()) : ((Fixed)p3.getX() - (Fixed)p1.getX());
+			bool p_closer_than_p3_to_p1 = (distance_from_p_to_p1) < (distance_from_p3_to_p1);
+			return (p3_same_side_p && p_closer_than_p3_to_p1);
 		}
 	}
 	Fixed	expected_y_for_p = get_y_for_x(p1, p2, p.getX());
-	Point middle = Point((p1.getX() + p2.getX() + p3.getX()) / 3,(p1.getY() + p2.getY() + p3.getY()) / 3);
-	if (get_y_for_x(p1, p2, middle.getX()) > middle.getY())
-		return ((p.getY() - expected_y_for_p) <= Fixed(0));
-	return ((expected_y_for_p - p.getY()) <= Fixed(0));
+	//if the actual y is higher than the expected y then it can only be correct if the line is below the middle
+	//if the actual y is lower than the expected y then it can only be corect if the line is above the middle
+
+	Point	middle = Point((p1.getX() + p2.getX() + p3.getX()) / 3,(p1.getY() + p2.getY() + p3.getY()) / 3);
+	Fixed	expected_y_for_middle = get_y_for_x(p1, p2, middle.getX());
+	if (expected_y_for_middle > middle.getY()) {
+		//the middle point is located below the line of p1-p2
+		//return true if the actual y is lower than the expected y cause then it's below the line (which we want cause the line is above the middle)
+		return (p.getY() < expected_y_for_p);
+	}
+	else if (expected_y_for_middle < middle.getY()) {
+		//the middle point is located above the line of p1-p2
+		//return true if the actual y is higher than the expected y cause then it's above the line (which we want cause the line is below the middle)
+		return (p.getY() > expected_y_for_p);
+	}
+	//the middle point is located in the line of p1-2 thus the 'triangle' is a line 
+	return (false);
 }
 
 bool bsp(Point const a, Point const b, Point const c, Point const point) {
-	bool	onUnderA;
-	bool	onUnderB;
-	bool	onUnderC;
+	bool	underA;
+	bool	underB;
+	bool	underC;
 	//where A is the line opposite of the corner a, etc.
 
-	onUnderA = is_on_under(b, c, a, point);
-	onUnderB = is_on_under(c, a, b, point);
-	onUnderC = is_on_under(a, b, c, point);
-	return (onUnderA && onUnderB && onUnderC);
+	//'under' in this place meaning taking the middle of the triangle as the 'gravitational center'
+	//and the side of the line that that 'gravitational center' is on is the 'under' side
+	//yes this is a very convoluted explanation but my brain is mush after wrestling with logic
+	underA = is_under(b, c, a, point);
+	underB = is_under(c, a, b, point);
+	underC = is_under(a, b, c, point);
+	return (underA && underB && underC);
 }
--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -3,18 +3,18 @@
 int	main( void ) {
 	Point a = Point(0, 0);
 	Point b = Point(1, 1);
-	Point c = Point(0.5, 0);
-	Fixed test;
+	Point c = Point(0.99, 0);
+	Fixed test1;
 	Fixed test2;
-	test.setRawBits(85);
-	test2.setRawBits(86);
+	test1.setRawBits(85);
+	test2.setRawBits(1);
 	//(1/3) / 0.00390625 (=float value returned on setRawBits(1)) -> rawbits=85 + 1/3 -> not possible in int so becomes 85
-	Point point1 = Point(Fixed(test), Fixed(test));
-	Point point2 = Point(Fixed(test), Fixed(test2));
+	Point point1 = Point(test1, test1);
+	Point point2 = Point(test1, test1 - test2);
 	Point point3 = Point((float)1/(float)3, (float)1/(float)3);
-	Point point4 = Point(1, 1);
-	Point point5 = Point(-1, 1);
-	Point point6 = Point(0.5, 0);
+	Point point4 = Point(Fixed(1) - test2, Fixed(1) - test2 - test2);
+	Point point5 = Point(Fixed(-1) + test2, Fixed(1) - test2);
+	Point point6 = Point(Fixed((float)0.5), test2);
 	Point points[6] = {point1, point2, point3, point4, point5, point6};
 	for (int i = 0; i < 6; i++) {
 		std::cout << "is point (" << points[i].getX() << "," << points[i].getY() << ")";
