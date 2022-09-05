d6d83d6fb9f94587237e1540448e575d7586b91a
pdruart <pdruart@student.codam.nl>
2022-09-05 13:53:19 +0200

    added default constructor as per canonical form, but private

--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -8,14 +8,16 @@ private:
 	unsigned int	hit_points;
 	unsigned int	energy_points;
 	unsigned int	attack_damage;
+
+	ClapTrap(void);
 public:
 	/* Canonical */
-	ClapTrap(std::string name);
 	ClapTrap(ClapTrap const & src);
 	~ClapTrap(void);
 	ClapTrap &	operator=(ClapTrap const & rhs);
 
 	/* Specific */
+	ClapTrap(std::string name);
 	void	attack(const std::string& target);
 	void	takeDamage(unsigned int amount);
 	void	beRepaired(unsigned int amount);
